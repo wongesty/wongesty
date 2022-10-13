61f2ed3ac4cf6041381b5eebc731111bdf6b80e8
wongesty <115610298+wongesty@users.noreply.github.com>
2022-10-13 21:14:22 +0700

    Create priik.sh

---
+++ priik.sh
@@ -0,0 +1,13 @@
+piblic class wonge
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay wonge
+echo
+date
